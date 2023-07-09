c64ddc7709abc8981164c6d6c472ff255d92f618
Lyudov <dlyudov@gmail.com>
2023-07-09 15:26:55 +0300

    Create home page logic

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/user/user.service';
 
 @Component({
@@ -8,11 +9,20 @@ import { UserService } from 'src/app/user/user.service';
 })
 export class HeaderComponent {
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
 
   }
 
-get isLoggedIn():boolean{
-  return this.userService.isLogged
-}
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+  get firstName(): string {
+    return this.userService.user?.firstName || '';
+  }
+
+  logout(): void {
+    this.userService.logout();
+    this.router.navigate(['/']);
+  }
+
 }
